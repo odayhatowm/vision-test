6a656124da90881ee49c8489a7ab437bc5dc1808
odayhatowm <68478022+odayhatowm@users.noreply.github.com>
2024-08-28 22:48:18 +0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import Login from "./Pages/Login";
+import Contacts from "./Pages/Contacts";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import React from "react";
 
+const App = () => {
+  return (
+    <React.StrictMode>
+      <BrowserRouter>
+        <Routes>
+        <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/contacts" element={<Contacts />} />
+        </Routes>
+      </BrowserRouter>
+    </React.StrictMode>
+  );
+};
+
+export default App;
